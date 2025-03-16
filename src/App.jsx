4752f96367c00a13c1acfe9346e9852746240cee
Olexandra Ghordievska <maynarisochka@gmail.com>
2025-03-16 16:33:48 +0200

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 import { useEffect } from "react";
 import { fetchContacts } from "./redux/contactsOps";
-import { useDispatch } from "react-redux";
-import ErrorMessage from './components//ErrorMessage/ErrorMessage';
-import Loading from './components/Loading/Loading';
+import { useDispatch, useSelector } from "react-redux";
+import { selectError, selectLoading } from "./redux/selectors"; 
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import Loading from "./components/Loading/Loading";
 
 function App() {
   const dispatch = useDispatch();
+  const loading = useSelector(selectLoading); 
+  const error = useSelector(selectError); 
+
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
@@ -19,6 +23,8 @@ function App() {
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
+      {error && <ErrorMessage>Error!</ErrorMessage>} {}
+      {loading && !error && <Loading />} {}
       <ContactList />
     </div>
   );
